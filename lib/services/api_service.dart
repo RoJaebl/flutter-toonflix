5fcb24f85e78241ce3eb146e7eccf55662ba8a09
RoJaebl <qgs0924@gmail.com>
2023-11-19 15:59:15 +0900

    :hammer: Creat waitForWebToons functions

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,16 +1,27 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:toonflix/modules/webtoon_model.dart';
 
 class ApiService {
-  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
-  final String today = "today";
+  static const String baseUrl =
+      "https://webtoon-crawler.nomadcoders.workers.dev";
+  static const String today = "today";
 
-  void getTodaysToons() async {
+  static Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
     final resopnse = await http.get(url);
+
     if (resopnse.statusCode == 200) {
-      print(resopnse.body);
-      return;
+      final List<dynamic> webtoons = jsonDecode(resopnse.body);
+
+      for (var webtoon in webtoons) {
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+      }
+      return webtoonInstances;
     }
+
     throw Error();
   }
 }
